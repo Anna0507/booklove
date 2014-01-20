314c2cfc2d513152d12f5c060072dab6cb8cbf25
Johannes Seikowsky <seikowsky@googlemail.com>
2014-01-20 04:23:53 +0100

    taking mailer out

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -3,7 +3,6 @@ class EmailsController < ApplicationController
   	@email = Email.new(email_params)
   	if @email.save 
   		flash[:success] = "Your email has been sent. Thank you for your input. We'll take care of it ASAP."
-  		UserMailer.user_input(@email).deliver 
       # Call the mailer, call the relevant method. Then call deliver. 
       redirect_to root_path
   	else 
@@ -11,7 +10,6 @@ class EmailsController < ApplicationController
   		redirect_to root_path
   	end	
   end
-
 
   private
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,12 +28,5 @@ Angeltation::Application.configure do
   config.assets.debug = true
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'example.com',
-    user_name:            'seikowsky@gmail.com',
-    password:             'aufgehts1',
-    authentication:       'plain',
-    enable_starttls_auto: true  }
+  
   end
